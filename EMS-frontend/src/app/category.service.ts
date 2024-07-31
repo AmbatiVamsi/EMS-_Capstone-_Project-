921f0a1af7de1971c8a5b5c3868880c7ed86b846
AmbatiVamsi <vamsikrishna66631@gmail.com>
2024-07-31 16:57:13 +0530

    Add files via upload

---
+++ EMS-frontend/src/app/category.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Category {
+  categoryID: number;
+  name: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:9093'; 
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(`${this.apiUrl}/categories`, category);
+
+  }
+
+  updateCategory(category: Category): Observable<Category> {
+    return this.http.put<Category>(`${this.apiUrl}/categories/${category.categoryID}`, category);
+  }
+
+  deleteCategory(categoryID: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/categories/${categoryID}`);
+  }
+}
